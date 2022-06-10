0a91d056fdfc0a9dc4d4ddade3d804cc9ff49152
Maksin_Andreev <andrianov_maksim@outlook.com>
2022-06-10 08:41:06 +0300

    some code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
 ﻿
+
+string PrintArray(string[] printarray)
+{
+    string print = string.Empty;
+    for (int i = 0; i < printarray.Length; i++)
+    {
+        print += $"{printarray[i]} ";
+        print += System.Environment.NewLine;
+    }
+    return print;
+}
+
