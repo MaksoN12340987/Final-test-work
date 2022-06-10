a7603fa1e37bc9740fd6be838d82a93d6fa880c1
Maksin_Andreev <andrianov_maksim@outlook.com>
2022-06-10 19:59:04 +0300

    added a method for inputting data and displaying an array on the screen

--- Program.cs
+++ Program.cs
@@ -1,9 +1,29 @@
-﻿
+﻿int GetValueNullNatural(string text)
+{
+    string getValue = String.Empty;
+    int value = 0;
+    bool correct = false;
+    while (!correct)
+    {
+        bool control = false;
+        System.Console.Write($"Enter the {text}:");
+        getValue = System.Console.ReadLine()!;
+        control = int.TryParse(getValue, out value);
+        if (control == true)
+        {
+            if (value != 0)
+            {
+                correct = true;
+            }
+        }
+    }
+    return value;
+}
 
-string PrintArray(string[] printarray)
+string PrintArray(string[] printarray, int Length)
 {
     string print = string.Empty;
-    for (int i = 0; i < printarray.Length; i++)
+    for (int i = 0; i < Length; i++)
     {
         print += $"{printarray[i]} ";
         print += System.Environment.NewLine;
@@ -11,3 +31,8 @@ string PrintArray(string[] printarray)
     return print;
 }
 
+void Main()
+{
+    string enter = "array size";
+    int size = GetValueNullNatural(enter);
+}
